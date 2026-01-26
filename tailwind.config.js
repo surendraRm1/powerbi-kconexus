c812d7caf30fbc0a6a221eba31a0d1d03d2cafea
surendraRm1 <sbnlmdsurendra@gmail.com>
2026-01-26 16:08:23 +0530

    Fix Tailwind ink color build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#FC8019", // Swiggy orange
+        primary: "#FC8019",
         dark: "#1c1c1c",
+        navy: {
+          800: "#1a3a8a",
+          900: "#1a2b5d",
+        },
+        ink: {
+          500: "#4b5563",
+        },
+        'openbi-green': "#22c55e",
       },
     },
   },
